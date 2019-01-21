a90fadb7bd35a38c8397097d63b9db7120db5f28
sylmz <soylemezali42@gmail.com>
2019-01-21 17:18:06 +0300

    userPostsById query si userById ile değiştirildi.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,7 +6,7 @@ function postById(parent, args, ctx, info) {
   return ctx.db.query.post({ where: { id: args.postId } }, info);
 }
 
-function userById(parent, args, ctx, info) {
+function userPostsById(parent, args, ctx, info) {
   return ctx.db.query.user({ where: { id: args.userId } }, info);
 }
 
@@ -17,6 +17,6 @@ function imageById(parent, args, ctx, info) {
 module.exports = {
   postList,
   postById,
-  userById,
+  userPostsById,
   imageById
 };
